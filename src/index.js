5a16ba156db03734a2c9833cc9b5f118d42fbcba
Ievgen Shakin <shakinzhenya@gmail.com>
2022-01-18 00:11:24 +0200

    +-

--- src/index.js
+++ src/index.js
@@ -5,22 +5,64 @@ import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.search-form'),
-  input: document.querySelector('input'),
+  input: document.querySelector('.input'),
   srcBtn: document.querySelector('.searchBtn'),
   loadMoreBtn: document.querySelector('.load-more'),
+  gallery: document.querySelector('.gallery'),
 }
+
 let name = '';
 let pageNumber = 1;
 
-console.log(refs);
-
-refs.srcBtn.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
+  pageNumber = 1;
+  refs.loadMoreBtn.classList.add('is-hidden')
+  e.preventDefault()
+  name = refs.input.value.trim()
+  refs.gallery.innerHTML = ''
+
+  getImages(name, pageNumber).then((data) => {
+    // console.log(data)
+    if (!name) {
+      Notiflix.Notify.failure('Oops! Write something!');
+    }
+    else if (data.hits.length === 0) {
+      errorFunction()
+    }
+    else {
+      renderGallery(data.hits)
+      countFounding(data.totalHits)
+      if (data.hits.length >= 40) { refs.loadMoreBtn.classList.remove('is-hidden') };
+    }
+  })
+    .catch(error => console.log(error))
 }
 
-refs.loadMoreBtn.addEventListener('submit', onloadMoreSubmit);
+refs.loadMoreBtn.addEventListener('click', onloadMoreClick);
 
-function onloadMoreSubmit(e) {
+function onloadMoreClick() {
+  pageNumber += 1;
+  getImages(name, pageNumber).then((data) => {
+    // console.log(data)
+    if ((data.totalHits / pageNumber) < 40) {
+      endOfSearch()
+    }
+    else {
+      renderGallery(data.hits)
+      countFounding(data.totalHits)
+    }
 
+    // console.log(data.totalHits / pageNumber)
+
+  }).catch(error => console.log(error))
+}
+function errorFunction() { Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.') }
+
+function countFounding(pages) { Notiflix.Notify.success(`Hooray! We found ${pages} images.`) }
+
+function endOfSearch() {
+  refs.loadMoreBtn.classList.add('is-hidden');
+  Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
 }
--- src/js/get-images.js
+++ src/js/get-images.js
@@ -6,10 +6,11 @@ const URL = 'https://pixabay.com/api/';
 const KEY = '25250442-1f0e0fa9809b8d86f02acb467';
 
 async function getImages(name, page) {
-  try {
-    const response = await axios.get(`${URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
-    return response;
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await
+    axios.get(`${URL}/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+      .then(resp => resp.data)
+      .catch(error =>
+        console.log(error)
+      )
+  return response
 }
